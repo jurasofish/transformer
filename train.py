68cc8c49dd0b2ac644e66bfb0d58b4612870d0d3
jurasofish <jurasofish@users.noreply.github.com>
2018-04-09 21:27:48 +1000

    added function to generate multivariate time series x and y

--- train.py
+++ train.py
@@ -18,6 +18,54 @@ from pprint import pprint
 import random, math
 import matplotlib.pyplot as plt
 
+
+def generate_x_y_data_v1(isTrain, batch_size):
+    """
+    Data for exercise 1.
+    returns: tuple (X, Y)
+        X is a sine and a cosine from 0.0*pi to 1.5*pi
+        Y is a sine and a cosine from 1.5*pi to 3.0*pi
+    Therefore, Y follows X. There is also a random offset
+    commonly applied to X an Y.
+    The returned arrays are of shape:
+        (seq_length, batch_size, output_dim)
+        Therefore: (10, batch_size, 2)
+    For this exercise, let's ignore the "isTrain"
+    argument and test on the same data.
+    """
+    seq_length = 10
+
+    batch_x = []
+    batch_y = []
+    for _ in range(batch_size):
+        rand = random.random() * 2 * math.pi
+
+        sig1 = np.sin(np.linspace(0.0 * math.pi + rand,
+                                  3.0 * math.pi + rand, seq_length * 2))
+        x1 = sig1[:seq_length]
+        y1 = sig1[seq_length:]
+
+        x_ = np.array([x1, np.zeros_like(x1)])
+        y_ = np.array([y1])
+        x_, y_ = x_.T, y_.T
+
+        batch_x.append(x_)
+        batch_y.append(y_)
+
+    batch_x = np.array(batch_x)
+    batch_y = np.array(batch_y)
+    # shape: (batch_size, seq_length, output_dim)
+    
+    '''
+    batch_x = np.array(batch_x).transpose((1, 0, 2))
+    batch_y = np.array(batch_y).transpose((1, 0, 2))
+    # shape: (seq_length, batch_size, output_dim)
+    '''
+
+    return batch_x, batch_y
+    
+
+
 class Graph():
     def __init__(self, is_training=True):
         self.graph = tf.Graph()
